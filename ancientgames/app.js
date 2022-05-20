d6684694c6f594de1e2c34b3b9d2cd6f6aba571a
MUGESHBABU-MAP <56474500+MUGESHBABU-MAP@users.noreply.github.com>
2022-05-20 12:17:07 +0530

    Updated root in ancient games. Removed ad code.
    Updated root in ancient games. Removed ad code.

--- ancientgames/app.js
+++ ancientgames/app.js
@@ -2,6 +2,10 @@ var start = 0;
 var root = location.protocol + '//' + location.host+'/mugeshbabu-map/ancientgames';
 if(location.pathname=='/'){
     var root = location.protocol + '//' + location.host;
+} else if(location.pathname=='ancientgames'){
+    var root = location.protocol + '//' + location.host+'/ancientgames';
+} else if(location.pathname=='/ancientgames'){
+    var root = location.protocol + '//' + location.host+'/ancientgames';
 }
 
 function startGame(){
